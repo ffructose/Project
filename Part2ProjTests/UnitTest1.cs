ae0035ebed030e33fa9844d4fe73aa924ac73ebf
Anna Hnatiuk <losers2004@gmail.com>
2025-11-19 00:24:46 +0100

    Part2 folder init

---
+++ Part2ProjTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace Part2ProjTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
